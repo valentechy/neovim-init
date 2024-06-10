21095c02c54ece050a8ccb6fb93bd981507560e0
Valentín Palacios <valentin.palacios@accenture.com>
2024-06-10 23:10:42 +0200

    Nvim tree

--- init.lua
+++ init.lua
@@ -83,6 +83,12 @@ require('lazy').setup({
   'hrsh7th/cmp-vsnip',
   'hrsh7th/vim-vsnip',
 
+  -- tree
+  'nvim-tree/nvim-tree.lua',
+  'nvim-tree/nvim-web-devicons',
+  'antosha417/nvim-lsp-file-operations',
+  'echasnovski/mini.base16',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
@@ -528,3 +534,34 @@ lspconfig.gopls.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+--
+
+-- Nvim Tree
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+-- OR setup with some options
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+local api = require("nvim-tree.api")
+local Event = api.events.Event
+api.events.subscribe(Event.TreeOpen, function()
+  api.tree.expand_all()
+end)
